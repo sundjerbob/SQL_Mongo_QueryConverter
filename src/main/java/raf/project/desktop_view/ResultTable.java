2b98b3970517ba9452aae07b47e896b95515b593
minnakovacevicc <mkovacevic8819rn@raf.rs>
2023-06-05 23:48:21 +0200

    dodata tabela za dinamicki prikaz resultSeta izvrsenih query-ja.

--- src/main/java/raf/project/desktop_view/ResultTable.java
+++ src/main/java/raf/project/desktop_view/ResultTable.java
@@ -1,14 +1,36 @@
 package raf.project.desktop_view;
 
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.List;
 
 public class ResultTable extends JTable {
 
-    //todo: ovde pisi tabelu M
-    @Override
-    public void paint(Graphics g) {
-        setPreferredSize(new Dimension(getParent().getWidth(), getParent().getHeight() / 2));
-        super.paint(g);
+    public ResultTable() {
+        // Initialize the table with an empty default model
+        setModel(new DefaultTableModel());
+    }
+
+    public void displayResultSet(List<List<String>> resultSet) {
+        DefaultTableModel model = (DefaultTableModel) getModel();
+
+        // Clear existing table data
+        model.setRowCount(0);
+        model.setColumnCount(0);
+
+        if (resultSet != null && !resultSet.isEmpty()) {
+            List<String> columnNames = resultSet.get(0);
+
+            // Set the column names
+            for (String columnName : columnNames) {
+                model.addColumn(columnName);
+            }
+
+            // Set the row data
+            for (int i = 1; i < resultSet.size(); i++) {
+                List<String> rowData = resultSet.get(i);
+                model.addRow(rowData.toArray());
+            }
+        }
     }
 }
